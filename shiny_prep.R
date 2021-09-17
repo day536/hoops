efa6dca6e8d1f432d0383d5918f004430bea6baf
day536 <day.536@osu.edu>
2021-09-17 08:37:44 -0700

    added some stats

--- shiny_prep.R
+++ shiny_prep.R
@@ -5,13 +5,14 @@ curated_player_data <- read_csv("https://raw.githubusercontent.com/day536/hoops/
 
 #Curate and prep for the app
 curated_player_data <- curated_player_data %>% 
+  mutate(ast_usagepercent = astpercent/usgpercent) %>% 
   group_by(pos) %>% 
-  mutate_at(vars(fg:bpm), funs("percentile" = rank(.)/length(.))) %>% 
+  mutate_at(vars(fg:bpm, ast_usagepercent), funs("percentile" = rank(.)/length(.))) %>% 
   ungroup()
 
 
 player_profile_vec <- c("player", "class", "season", "pos", "school")
-player_impact_vec <- c("usgpercent", "tspercent", "astpercent")
+player_impact_vec <- c("usgpercent", "tspercent", "astpercent", "ast_usagepercent", "tovpercent")
 four_factors_vec <- c("efgpercent", "orbpercent", "drbpercent", "blkpercent", "tovpercent")
 
 
