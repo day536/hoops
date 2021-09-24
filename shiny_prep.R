f2db96ba423cb3f0923c75d179c0753446e1f39b
day536 <day.536@osu.edu>
2021-09-24 09:05:25 -0700

    pushing final updates

--- shiny_prep.R
+++ shiny_prep.R
@@ -2,13 +2,15 @@ library(tidyverse)
 
 #Get that data boi
 curated_player_data <- read_csv("https://raw.githubusercontent.com/day536/hoops/main/curated_data/advanced.csv")
+hoop_math_shooting_data <- read_csv("https://raw.githubusercontent.com/day536/hoops/main/curated_data/hoop_math_player_bridge.csv")
 
 #Curate and prep for the app
 curated_player_data <- curated_player_data %>% 
+  left_join(hoop_math_shooting_data, by = c("player", "school", "season")) %>% 
   mutate(ast_usagepercent = astpercent/usgpercent) %>% 
   filter(fga >= 30) %>% 
   group_by(pos) %>% 
-  mutate_at(vars(fg:bpm, ast_usagepercent), funs("percentile" = rank(.)/length(.))) %>% 
+  mutate_at(vars(fg:bpm, percent_shots_at_rim:ast_usagepercent), funs("percentile" = rank(.)/length(.))) %>% 
   ungroup() %>% 
   mutate(tovpercent_percentile = 1 - tovpercent_percentile)
 
@@ -28,4 +30,6 @@ player_impact_data <- curated_player_data %>%
   select(matches(player_profile_vec) | g, mp, contains(player_impact_vec)) %>% 
   select(-contains("link"))
 
+#player_shooting_data <- curated_player_data %>% 
+  
 
