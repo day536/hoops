5cd16a5b5ec891c1cc9da4c2969702578a44e0ca
day536 <day.536@osu.edu>
2021-09-30 23:13:18 -0700

    pushing ff updates

--- shiny_prep.R
+++ shiny_prep.R
@@ -7,7 +7,11 @@ hoop_math_shooting_data <- read_csv("https://raw.githubusercontent.com/day536/ho
 #Curate and prep for the app
 curated_player_data <- curated_player_data %>% 
   left_join(hoop_math_shooting_data, by = c("player", "school", "season")) %>% 
-  mutate(ast_usagepercent = astpercent/usgpercent) %>% 
+  mutate(netrtg = ortg - drtg,
+         ast_usagepercent = astpercent/usgpercent,
+         fga_at_rim = round(percent_shots_at_rim * hm_fag, 0)/g,
+         fga_2pt_j = round(percent_shots_2pt_j * hm_fag, 0)/g,
+         fga_3pt = round(percent_of_shots_3pt * hm_fag, 0)/g) %>% 
   filter(fga >= 30) %>% 
   ##mutate_at(across(c("ts_percent","ft_percent"), map_dbl(.x = ., .f ~))
   #mutate(ts_percent = map_dbl(.x = ts_percent, .f = ~replace_na(x = .x, replace = 0))) %>% 
@@ -19,13 +23,20 @@ curated_player_data <- curated_player_data %>%
 
 player_profile_vec <- c("player", "class", "season", "pos", "school")
 player_impact_vec <- c("usgpercent", "tspercent", "astpercent", "ast_usagepercent", "tovpercent")
-four_factors_vec <- c("efgpercent", "orbpercent", "drbpercent", "blkpercent", "tovpercent")
+four_factors_vec <- c("netrtg", "efgpercent", "orbpercent", "drbpercent", "blkpercent", "tovpercent")#, "fta_fga", "ortg", "drtg")
+four_factors_off_vec <- c("netrtg", "ortg", "efgpercent", "orbpercent", "tovpercent", "fta_fga")#, "fta_fga", "ortg", "drtg")
+
 #shooting_vec <- c("shots_at_rim", "percent_at_rim", "2pt_j", "2pt_jumper", "x3fg")
 
 
 four_factors_data <- curated_player_data %>% 
   ungroup() %>% 
   select(matches(player_profile_vec) | g, mp, contains(four_factors_vec), -contains("hoop_math")) %>% 
+  select(-contains("link"))
+
+four_factors_off_data <- curated_player_data %>% 
+  ungroup() %>% 
+  select(matches(player_profile_vec) | g, mp, contains(four_factors_off_vec), -contains("hoop_math")) %>% 
   select(-contains("link"))
 
 player_impact_data <- curated_player_data %>% 
@@ -40,14 +51,17 @@ player_shooting_acc_data <- curated_player_data %>%
          efgpercent, efgpercent_percentile,
          #percent_shots_at_rim,
          #percent_shots_at_rim_percentile,
+         fga_at_rim,
          fg_percent_at_rim,
          fg_percent_at_rim_percentile,
          #percent_shots_2pt_j,
          #percent_shots_2pt_j_percentile,
+         fga_2pt_j,
          fg_percent_2pt_jumpers,
          fg_percent_2pt_jumpers_percentile,
          #percent_of_shots_3pt,
          #percent_of_shots_3pt_percentile,
+         fga_3pt,
          x3fg_percent,
          x3fg_percent_percentile)
 
@@ -55,14 +69,17 @@ player_shooting_freq_data <- curated_player_data %>%
   ungroup() %>% 
   select(matches(player_profile_vec) | g, mp, -contains("link"), -contains("hoop_math"),
          efgpercent, efgpercent_percentile,
+         fga_at_rim,
          percent_shots_at_rim,
          percent_shots_at_rim_percentile,
          #fg_percent_at_rim,
          #fg_percent_at_rim_percentile,
+         fga_2pt_j,
          percent_shots_2pt_j,
          percent_shots_2pt_j_percentile,
          #fg_percent_2pt_jumpers,
          #fg_percent_2pt_jumpers_percentile,
+         fga_3pt,
          percent_of_shots_3pt,
          percent_of_shots_3pt_percentile) #,
          #x3fg_percent,
